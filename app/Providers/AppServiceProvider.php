abc4070872b3e39c3e5f707c9ba0690c1bd2f2ea
Xurshiddd <marifovxurshid800@gmail.com>
2026-02-04 18:16:23 +0500

    as

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -28,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
 
     protected function configureDefaults(): void
     {
+        Schema::defaultStringLength(255);
         Date::use(CarbonImmutable::class);
 
         DB::prohibitDestructiveCommands(
