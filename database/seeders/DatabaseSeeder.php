ce2fd0346e749f0458069b719eb945d3594adef6
Xurshiddd <marifovxurshid800@gmail.com>
2026-03-04 10:41:17 +0500

    dfgd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,40 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     { 
-        for ($i = 1; $i <= 10; $i++) {
-            Group::create([
-                'name' => "Group $i",
-                'code' => 1000 + $i,
-                'education_lang' => 'Uzbek',
-                'education_form' => 'Full-time',
-                'education_type' => 'Bachelor',
-            ]);
-            Speciality::create([
-                'name' => "Speciality $i",
-                'code' => 2000 + $i,
-            ]);
-        }
-        User::factory(1000)->create();
-        User::create([
-            'name' => 'Admin',
-            'email' => 'marifovxurshid8@gmail.com',
-            'login' => 454546464,
-            'password' => bcrypt('RootPassword'),
-            'role' => 'admin',
-        ]);
-        User::create([
-            'name' => 'Dildora Ikromovna',
-            'email' => 'dildora@ttysi.com',
-            'login' => 123456789,
-            'password' => bcrypt('RootPassword'),
-            'role' => 'psiholog',
-        ]);
-        User::create([
-            'name' => 'Student',
-            'email' => 'student@ttysi.com',
-            'login' => 123456789,
-            'password' => bcrypt('RootPassword'),
-            'role' => 'student',
-        ]);
+        // for ($i = 1; $i <= 10; $i++) {
+        //     Group::create([
+        //         'name' => "Group $i",
+        //         'code' => 1000 + $i,
+        //         'education_lang' => 'Uzbek',
+        //         'education_form' => 'Full-time',
+        //         'education_type' => 'Bachelor',
+        //     ]);
+        //     Speciality::create([
+        //         'name' => "Speciality $i",
+        //         'code' => 2000 + $i,
+        //     ]);
+        // }
+        // User::factory(1000)->create();
     }
 }
